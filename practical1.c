4bcef148c85eb0882f6bbf29975f1582fa00d9b9
PriyalAmeta <priyalametatcet@gmail.com>
2026-07-22 08:00:27 +0530

    Added first occurrence position practical1.c

--- practical1.c
+++ practical1.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 
 int main() {
-    int n, x, count = 0;
-    
+    int n, x;
+    int count = 0;
+    int first = -1;
+
     printf("Enter size of array: ");
     scanf("%d", &n);
 
     int arr[n];
+
     for (int i = 0; i < n; i++) {
         printf("Enter an element: ");
         scanf("%d", &arr[i]);
     }
 
-    printf("Enter the element to search for (x): ");
+    printf("Enter the element to search for: ");
     scanf("%d", &x);
 
     for (int i = 0; i < n; i++) {
         if (arr[i] == x) {
             count++;
+
+            if (first == -1) {
+                first = i;
+            }
         }
     }
 
     if (count == 0) {
-        printf("not found\n");
+        printf("Element not found.\n");
     } else {
-        printf("no. of repetations: %d\n", count);
+        printf("Number of occurrences: %d\n", count);
+        printf("First occurrence position: %d\n", first);
     }
 
     return 0;
